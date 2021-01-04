a5d9736f759ce74b25fe4058c31753ab06be7766
Vamsi Tallapudi <vamsi@coderefer.com>
2021-01-04 15:18:39 +0530

    merge two sorted lists recursive version

--- programming/leetcode/january_challenge_2021/MergeTwoSortedLists.py
+++ programming/leetcode/january_challenge_2021/MergeTwoSortedLists.py
@@ -13,22 +13,3 @@ class Solution:
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
-
-
-def insert(head, data):
-    if not head:
-        return ListNode(data)
-    while head.next:
-        head = head.next
-    head.next = ListNode(data)
-    return head
-
-
-head1 = ListNode(1)
-a = insert(head1, 2)
-b = insert(a, 4)
-head2 = ListNode(1)
-c = insert(head2, 3)
-d = insert(c, 4)
-
-print(print_ll(Solution().mergeTwoLists(head1, head2)))
