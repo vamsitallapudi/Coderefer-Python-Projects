a7acc682251661e296c64533f4a85d47e6eedda2
Vamsi Tallapudi <vamsi@coderefer.com>
2021-02-21 10:28:57 +0530

    Merge Strings Alternatively

---
+++ programming/leetcode/contests/weekly/Contest229/MergeStringsAlternately.py
@@ -0,0 +1,9 @@
+from itertools import zip_longest
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return "".join(a+b for a,b in zip_longest(word1, word2, fillvalue=''))
+
+
+print(Solution().mergeAlternately("abcd", "pq"))
